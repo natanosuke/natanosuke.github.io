7659ea67a8f35006eb33dc43fa7afbdce8fbdfd0
dbdaxznpks <dbdaxznpks@gmail.com>
2024-12-30 22:30:44 +0900

    最新記事の投稿

--- js/filelist.js
+++ js/filelist.js
@@ -1,5 +1,11 @@
 const articles = [
     {
+        title: "サンタクロースラリーとは？",
+        subtitle: "年末年始の株式市場アノマリーを徹底解説",
+        date: "december 30, 2024",
+        link: "../posts/post2.html"
+    },
+    {
         title: "初めまして！「なたり資産倍増計画」へようこそ",
         subtitle: "さあ、共に新しい一歩を踏み出しましょう！",
         date: "december 29, 2024",
