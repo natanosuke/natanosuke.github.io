74d8abbd5f692718f5e9bcaab80c8e4c9cebb4fa
dbdaxznpks <dbdaxznpks@gmail.com>
2025-01-12 01:51:30 +0900

    プライバシーポリシー全適用と、楽天証券ツール記事投稿

--- js/filelisttools.js
+++ js/filelisttools.js
@@ -1,5 +1,11 @@
 const articles = [
     {
+        title: "松井証券",
+        subtitle: "手数料の安さと初心者向けサポートが充実した信頼の証券会社",
+        date: "january 12, 2025",
+        link: "../tools/tool3.html"
+        },
+    {
         title: "楽天証券",
         subtitle: "手数料が安く特典豊富！楽天ポイントを活用できたり初心者に優しい証券会社",
         date: "December 31, 2024",
