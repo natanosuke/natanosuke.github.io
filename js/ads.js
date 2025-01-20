b78dd6ba08cf56b19190eb0ab5bce54931bc927b
dbdaxznpks <dbdaxznpks@gmail.com>
2025-01-20 20:42:41 +0900

    マネックス証券アフィリエイトの適用

---
+++ js/ads.js
@@ -0,0 +1,27 @@
+     // サイドバーの末尾に広告を挿入
+document.addEventListener("DOMContentLoaded", function () {
+    const adCode = `
+      <div class="ad-container" style="margin: 20px 0; text-align: center;">
+      <a href="https://h.accesstrade.net/sp/cc?rk=0100kwxh00o8yg" rel="nofollow" referrerpolicy="no-referrer-when-downgrade"><img src="https://h.accesstrade.net/sp/rr?rk=0100kwxh00o8yg" alt="株・投資信託ならネット証券のマネックス" border="0" /></a>
+      </div>
+    `;
+  
+    
+    const mainContent = document.querySelector('#yearly-profit');
+    if (mainContent) {
+      mainContent.insertAdjacentHTML('beforeend', adCode);
+    }
+    });
+    
+      // サイドバーの末尾に広告を挿入
+document.addEventListener("DOMContentLoaded", function () {
+    const adCode = `
+      <div class="ad-container" style="margin: 20px 0; text-align: center;">
+      <a href="https://h.accesstrade.net/sp/cc?rk=0100kwxh00o8yg" rel="nofollow" referrerpolicy="no-referrer-when-downgrade"><img src="https://h.accesstrade.net/sp/rr?rk=0100kwxh00o8yg" alt="株・投資信託ならネット証券のマネックス" border="0" /></a>
+      </div>
+    `;
+      const mainContent = document.querySelector('.sidebar-heading2');
+      if (mainContent) {
+        mainContent.insertAdjacentHTML('beforeend', adCode);
+      }
+    });
